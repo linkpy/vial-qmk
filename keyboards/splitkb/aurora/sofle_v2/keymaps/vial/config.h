69dfd859228a7f1cf76b4644c072a5cf84e36350
Estel <linkpydev@gmail.com>
2023-08-10 23:30:05 +0200

    huh

---
+++ keyboards/splitkb/aurora/sofle_v2/keymaps/vial/config.h
@@ -0,0 +1,28 @@
+/* Copyright 2023 splitkb.com <support@splitkb.com>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#pragma once
+
+#define VIAL_KEYBOARD_UID {0x30, 0xE9, 0x5C, 0xC4, 0xB2, 0x13, 0x7C, 0xC9}
+#define VIAL_UNLOCK_COMBO_ROWS { 0, 4, 0 }
+#define VIAL_UNLOCK_COMBO_COLS { 0, 1, 5 }
+
+#define RGB_DISABLE_WHEN_USB_SUSPENDED
+#define RGB_MATRIX_KEYPRESSES
+#define ENABLE_RGB_MATRIX_SOLID_REACTIVE_SIMPLE
+#define ENABLE_RGB_MATRIX_SOLID_SPLASH
+#define ENABLE_RGB_MATRIX_RAINBOW_BEACON
+#define ENABLE_RGB_MATRIX_CYCLE_LEFT_RIGHT
